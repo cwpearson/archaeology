1cee38780831c721f66e24db4f131c832f3258a3
Carl Pearson <pearson@illinois.edu>
2017-02-16 09:17:47 -0600

    adler test

--- archaeology/adler/adler_test.go
+++ archaeology/adler/adler_test.go
@@ -1,18 +1,32 @@
 package adler
 
 import (
+	"bytes"
+	"fmt"
 	"testing"
 
-	assert "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRecurrence(t *testing.T) {
-	data := []byte(`These are definitely some bytes`)
+	data1 := []byte(`These are definitely some bytes`)
+	data2 := []byte(`These are similar         bytes`)
 
-	a := NewSum(data[3:5], 3)
-	b := NewSum(data[2:4], 2)
-	b.Recurrence(data[4], data[2])
+	windowSize := 3
+	a := NewSum(data1[0:windowSize], 0)
+	b := NewSum(data2[0:windowSize], 0)
+	for s := 0; s < len(data2)-windowSize; s++ {
+		e := s + windowSize
 
-	assert.Equal(t, a.Current(), b.Current(), "Should be equal")
+		if bytes.Equal(data1[s:e], data2[s:e]) {
+			assert.Equal(t, a.Current(), b.Current(), "Expected hashes of \""+string(data1[s:e])+"\" to match")
+		}
+
+		fmt.Printf("%s %s %X %X %t\n", string(data1[s+1:e+1]), string(data2[s+1:e+1]), a.Current(), b.Current(), a.Current() == b.Current())
+
+		a.Recurrence(data1[e], data1[s])
+		b.Recurrence(data2[e], data2[s])
+
+	}
 
 }
