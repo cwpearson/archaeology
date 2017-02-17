36d90c427e71226ddf3963d2ffbcc117ba522634
Carl Pearson <pearson@illinois.edu>
2017-02-17 08:12:39 -0600

    .

--- cmd/diff.go
+++ cmd/diff.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"os"
@@ -13,6 +14,43 @@ import (
 
 func init() {
 	RootCmd.AddCommand(diffCmd)
+}
+
+func compareBlocks(blockSize int, f1 *os.File, f1BlockOffsets []int64, f2 *os.File, f2BlockOffset int64) ([]int64, error) {
+	f2Buf := make([]byte, blockSize)
+	_, err := f2.Seek(f2BlockOffset, io.SeekStart)
+	if err != nil {
+		return nil, err
+	}
+	n, err := f2.Read(f2Buf)
+	if err != nil && err != io.EOF {
+		return nil, err
+	}
+	f2Buf = f2Buf[:n]
+	if err == io.EOF {
+	}
+
+	matches := []int64{}
+	f1Buf := make([]byte, blockSize)
+	for _, f1BlockOffset := range f1BlockOffsets {
+		f1Buf = f1Buf[:cap(f1Buf)]
+		_, err := f1.Seek(f1BlockOffset, io.SeekStart)
+		if err != nil {
+			return nil, err
+		}
+		n, err = f1.Read(f1Buf)
+		if err != nil && err != io.EOF {
+			return nil, err
+		}
+		f1Buf = f1Buf[:n]
+		if err == io.EOF {
+		}
+
+		if bytes.Equal(f1Buf, f2Buf) {
+			matches = append(matches, f1BlockOffset)
+		}
+	}
+	return matches, nil
 }
 
 var diffCmd = &cobra.Command{
@@ -43,8 +81,8 @@ var diffCmd = &cobra.Command{
 		f1Size := fi.Size()
 
 		start := time.Now()
-		checksums := []uint32{}
-		firstOffsets := map[uint32]int64{}
+		checksums := map[uint32][]int64{} // map of file1 block checksums to offsets in file1
+
 		buf := make([]byte, blockSize)
 
 		for k := int64(0); k < f1Size; k += blockSize {
@@ -63,10 +101,8 @@ var diffCmd = &cobra.Command{
 
 			s := adler.NewSum(buf, uint64(k))
 			checksum := s.Current()
-			checksums = append(checksums, checksum)
-			if _, ok := firstOffsets[checksum]; !ok {
-				firstOffsets[checksum] = k
-			}
+			checksums[checksum] = append(checksums[checksum], k)
+
 			if err == io.EOF {
 				log.Warn("Reached end of file1")
 				break
@@ -93,15 +129,30 @@ var diffCmd = &cobra.Command{
 
 		s := adler.NewSum(buf, uint64(0))
 
+		endPrevMatch := int64(-1)
 		for k := int64(0); k < f2Size; k++ {
 			l := k + blockSize
 
 			checksum := s.Current()
-			offset, ok := firstOffsets[checksum]
-			if ok {
-				fmt.Printf("file2 offset %d checksum matches file1 offset %d\n", k, offset)
+			if offsets, ok := checksums[checksum]; ok {
+				// fmt.Printf("file2 block checksum (%d-%d) matches file1 block offsets %v\n", k, l, offsets)
+
+				// Actually compare the blocks
+				matches, err := compareBlocks(blockSize, f1, offsets, f2, k)
+				if err != nil {
+					log.Fatal(err)
+				}
+				if len(matches) > 0 {
+					if k > endPrevMatch {
+						fmt.Printf("New data in file2 at %d\n", k)
+					}
+					fmt.Printf("file2 block (%d-%d) matches file1 offsets %v\n", k, l, matches)
+					endPrevMatch = l
+				}
+
+				endPrevMatch = l
 			} else {
-				// fmt.Printf("New checksum at offset %d\n", k)
+
 			}
 
 			f2.Seek(l, io.SeekStart)
